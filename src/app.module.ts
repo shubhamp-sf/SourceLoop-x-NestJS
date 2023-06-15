834a32a199327128b8f7be21e470ed4f3e4f91f0
Shubham P <shubham.prajapat@sourcefuse.com>
2023-06-15 15:54:57 +0530

    added middleware for sending requests to lb4 app's request handler

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoopBackMiddleware } from './lb4.middleware';
 
 @Module({
   imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoopBackMiddleware).forRoutes('*');
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,45 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  ApplicationConfig,
+  AuthenticationServiceApplication,
+} from '@sourceloop/authentication-service/dist/application';
+import { juggler } from '@loopback/repository';
 
+process.env.JWT_SECRET = 'secret';
+process.env.JWT_ISSUER = 'sourcefuse';
+process.env.USER_TEMP_PASSWORD = 'test123!@#';
+global.authService = null;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config: ApplicationConfig = {
+    rest: {
+      listenOnStart: false,
+    },
+  };
+  const authService = new AuthenticationServiceApplication(config);
+
+  const authDb = new juggler.DataSource({
+    name: 'AuthDB',
+    connector: 'postgresql',
+    host: 'localhost',
+    port: '6000',
+    user: 'postgres',
+    password: 'super-secret',
+    database: 'postgres',
+    schema: 'main',
+  });
+  authService.dataSource(authDb);
+
+  const authCache = new juggler.DataSource({
+    name: 'AuthCache',
+    connector: 'kv-memory',
+  });
+  authService.dataSource(authCache);
+
+  await authService.boot();
+  await authService.start();
+  global.authService = authService;
+  await app.listen(4000);
 }
 bootstrap();
